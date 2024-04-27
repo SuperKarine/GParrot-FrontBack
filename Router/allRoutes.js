5ce788c38c7cb77a886121269aad000ea14f6ff2
SuperKarine <119334722+SuperKarine@users.noreply.github.com>
2024-04-27 22:30:58 +0200

    Ajout de la page galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,8 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/formulaire", "formulaire.html", "/pages/formulaire.html"),
     
 ];
 
